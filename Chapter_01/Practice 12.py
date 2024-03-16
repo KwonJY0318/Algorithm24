97ba7b7d1bdc914accb33b04c2eee300df11e624
KwonJY0318 <wnsdud7530@naver.com>
2024-03-17 00:02:45 +0900

    Updated Chapter 01 Practice file

--- Chapter_01/Practice 12.py
+++ Chapter_01/Practice 12.py
@@ -2,9 +2,8 @@
 # 배열에서 가장 가까운 두 항목의 거리를 찾아 반환하는 알고리즘
 # 학번 202110075 권준영 // 수정해야함
 
-list = [8,7,2,4,1]
+list = [11,59,3,8,1]
 xlist = sorted (list)
-ylist = min+[xlist{i, i+1}]
 
 print(xlist)
-print(ylist)
+print(xlist[0], xlist[1])
