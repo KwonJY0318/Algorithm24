6dbc788ca36f627d5fce3b56d4d6bd8f3d320be7
KwonJY0318 <wnsdud7530@naver.com>
2024-04-24 15:36:41 +0900

    practice4.05

---
+++ P4.05.py
@@ -0,0 +1,16 @@
+def insertion_sort(A) :
+    n = len(A)
+
+    for i in range(1, n):
+        key = A[i]
+        j = i-1
+        while j>=0 and A[j]> key :
+            A[j+1] = A[j]
+            j = j-1
+        A[j +1 ] = key
+        print("Step:", A, i)
+
+data = [5, 3, 8, 4, 9, 1, 6, 2, 7]
+print("Original: ", data)
+insertion_sort(data)
+print("Insertion: ", data)
